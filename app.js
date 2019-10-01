74a4b46ee3e325211b4d1a142a11d8d197244039
KRYSTAL MADRINAN <KRYSTAL.MADRINAN@GMAIL.COM>
2019-10-01 14:15:33 -0700

    added js and css file plus the first question

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var userName = prompt('What\'s your name?');
+
+var myName = prompt('Hello ' + userName + '! ' + 'Is my name Karen?')
+myName = myName.toLowerCase();
+
+if (myName === 'no') {
+  alert('Correct! My name isn not Karn, it\'s Krystal.');
+} else {
+  alert('No! My name is not Karen, it\'s Krystal.');
+}
+
+
