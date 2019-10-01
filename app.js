dde5c3bb9c7dead4a2f2cbe3afa19bd29dce9be2
KRYSTAL MADRINAN <KRYSTAL.MADRINAN@GMAIL.COM>
2019-10-01 14:19:02 -0700

    fixed a typo in question

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var myName = prompt('Hello ' + userName + '! ' + 'Is my name Karen?')
 myName = myName.toLowerCase();
 
 if (myName === 'no') {
-  alert('Correct! My name isn not Karn, it\'s Krystal.');
+  alert('Correct! My name is not Karen, it\'s Krystal.');
 } else {
   alert('No! My name is not Karen, it\'s Krystal.');
 }
